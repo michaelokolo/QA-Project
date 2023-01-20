25d2bb2a4cbb479fd86ae30299241dda4c1d7d56
Michael Okolo <michaelokolo62@yahoo.com>
2023-01-20 17:36:08 -0500

    completed the js

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const questions = document.querySelectorAll('.question');
 
 
 questions.forEach((question)=>{
- const btn = question.querySelector('.question-btn')
+ const btn = question.querySelector('.question-btn');
+ btn.addEventListener('click', ()=>{
+    question.classList.toggle('show-text');
+ });
 })
